f0805053e8b328b3c0ded09e38d2f17a3ab02228
Greg Olmschenk <golmschenk@gmail.com>
2018-04-15 20:25:40 -0400

    Allow for settings permutations to be created by setting list

--- settings.py
+++ settings.py
@@ -1,6 +1,8 @@
 """
 General settings.
 """
+from copy import deepcopy
+
 
 class Settings():
     def __init__(self):
@@ -32,3 +34,31 @@ class Settings():
 
         self.load_model_path = None
         self.should_save_models = False
+
+
+def convert_to_settings_list(settings):
+    """
+    Creates permutations of settings for any setting that is a list.
+    This function is black magic. Beware.
+    """
+    settings_list = [settings]
+    next_settings_list = []
+    any_contains_list = True
+    while any_contains_list:
+        any_contains_list = False
+        for settings in settings_list:
+            contains_list = False
+            for attribute_name, attribute_value in vars(settings).items():
+                if isinstance(attribute_value, (list, tuple)):
+                    for value in attribute_value:
+                        settings_copy = deepcopy(settings)
+                        setattr(settings_copy, attribute_name, value)
+                        next_settings_list.append(settings_copy)
+                    contains_list = True
+                    any_contains_list = True
+                    break
+            if not contains_list:
+                next_settings_list.append(settings)
+        settings_list = next_settings_list
+        next_settings_list = []
+    return settings_list
--- srgan.py
+++ srgan.py
@@ -14,7 +14,7 @@ from torch.utils.data import DataLoader
 import torch
 
 from coefficient_models import observation_count, Generator, MLP
-from settings import Settings
+from settings import Settings, convert_to_settings_list
 from data import ToyDataset, MixtureModel, seed_all
 from presentation import generate_display_frame
 from training_functions import dnn_training_step, gan_training_step
@@ -151,49 +151,39 @@ def run_srgan(settings):
         torch.save(G.state_dict(), os.path.join(trial_directory, 'G_model.pth'))
 
 
-for labeled_dataset_seed in shuffled([0]):#, 3, 4, 5, 6]):
-    for labeled_dataset_size in shuffled([100]):#, 10, 30, 100, 300, 1000]):
-        for mean_offset in shuffled([2]):
-            for unlabeled_multiplier in shuffled([1e0]):
-                for fake_multiplier in shuffled([1e-1]):
-                    for gradient_penalty_multiplier in shuffled([1e1]):
-                        for learning_rate in shuffled([1e-5]):
-                            for unlabeled_loss_order in shuffled([2]):
-                                for fake_loss_order in shuffled([1]):
-                                    for generator_loss_order in shuffled([2]):
-                                        seed_all(0)
-                                        scale_multiplier = 1e0
-                                        fake_multiplier = fake_multiplier * scale_multiplier
-                                        unlabeled_multiplier = unlabeled_multiplier * scale_multiplier
-                                        settings_ = Settings()
-                                        settings_.fake_loss_multiplier = fake_multiplier
-                                        settings_.unlabeled_loss_multiplier = unlabeled_multiplier
-                                        settings_.steps_to_run = 500000
-                                        settings_.learning_rate = learning_rate
-                                        settings_.labeled_dataset_size = labeled_dataset_size
-                                        settings_.gradient_penalty_multiplier = gradient_penalty_multiplier
-                                        settings_.norm_loss_multiplier = 0
-                                        settings_.mean_offset = mean_offset
-                                        settings_.fake_loss_order = fake_loss_order
-                                        settings_.unlabeled_loss_order = unlabeled_loss_order
-                                        settings_.generator_loss_order = generator_loss_order
-                                        settings_.generator_training_step_period = 1
-                                        settings_.should_save_models = True
-                                        settings_.labeled_dataset_seed = labeled_dataset_seed
-                                        settings_.load_model_path = '/home/golmschenk/srgan/logs/detachall ul1e0 fl1e-1 le100 gp1e1 bg1e0 lr1e-5 nl0 gs1 ls0 u2f1g2 l y2018m04d15h14m14s10'
-                                        trial_name = 'detachall c'
-                                        trial_name += ' ul{:e}'.format(unlabeled_multiplier)
-                                        trial_name += ' fl{:e}'.format(fake_multiplier)
-                                        trial_name += ' le{}'.format(settings_.labeled_dataset_size)
-                                        trial_name += ' gp{:e}'.format(settings_.gradient_penalty_multiplier)
-                                        trial_name += ' bg{:e}'.format(settings_.mean_offset)
-                                        trial_name += ' lr{:e}'.format(settings_.learning_rate)
-                                        trial_name += ' nl{}'.format(settings_.norm_loss_multiplier)
-                                        trial_name += ' gs{}'.format(settings_.generator_training_step_period)
-                                        trial_name += ' ls{}'.format(settings_.labeled_dataset_seed)
-                                        trial_name += ' u{}f{}g{}'.format(settings_.unlabeled_loss_order,
-                                                                          settings_.fake_loss_order,
-                                                                          settings_.generator_loss_order)
-                                        trial_name += ' l' if settings_.load_model_path else ''
-                                        settings_.trial_name = clean_scientific_notation(trial_name)
-                                        run_srgan(settings_)
+if __name__ == '__main__':
+    seed_all(0)
+    settings_ = Settings()
+    settings_.unlabeled_loss_multiplier = 1e0
+    settings_.fake_loss_multiplier = 1e-1
+    settings_.steps_to_run = 500000
+    settings_.learning_rate = 1e-5
+    settings_.labeled_dataset_size = 100
+    settings_.gradient_penalty_multiplier = 1e1
+    settings_.norm_loss_multiplier = 0
+    settings_.mean_offset = 1
+    settings_.unlabeled_loss_order = 2
+    settings_.fake_loss_order = 1
+    settings_.generator_loss_order = 2
+    settings_.generator_training_step_period = 1
+    settings_.should_save_models = True
+    settings_.labeled_dataset_seed = 0
+    settings_.load_model_path = '/home/golmschenk/srgan/logs/detachall ul1e0 fl1e-1 le100 gp1e1 bg1e0 lr1e-5 nl0 gs1 ls0 u2f1g2 l y2018m04d15h14m14s10'
+    settings_list = convert_to_settings_list(settings_)
+    for settings_ in settings_list:
+        trial_name = 'detachall c'
+        trial_name += ' ul{:e}'.format(settings_.unlabeled_loss_multiplier)
+        trial_name += ' fl{:e}'.format(settings_.fake_loss_multiplier)
+        trial_name += ' le{}'.format(settings_.labeled_dataset_size)
+        trial_name += ' gp{:e}'.format(settings_.gradient_penalty_multiplier)
+        trial_name += ' bg{:e}'.format(settings_.mean_offset)
+        trial_name += ' lr{:e}'.format(settings_.learning_rate)
+        trial_name += ' nl{}'.format(settings_.norm_loss_multiplier)
+        trial_name += ' gs{}'.format(settings_.generator_training_step_period)
+        trial_name += ' ls{}'.format(settings_.labeled_dataset_seed)
+        trial_name += ' u{}f{}g{}'.format(settings_.unlabeled_loss_order,
+                                          settings_.fake_loss_order,
+                                          settings_.generator_loss_order)
+        trial_name += ' l' if settings_.load_model_path else ''
+        settings_.trial_name = clean_scientific_notation(trial_name)
+        run_srgan(settings_)
