8807af9354f919ce464e123546f174f0d13e19b4
Greg Olmschenk <golmschenk@gmail.com>
2018-04-17 16:44:28 -0400

    Use DCGAN that was available online for age

--- age_models.py
+++ age_models.py
@@ -1,77 +1,66 @@
-import torch
-from torch.autograd import Variable
-from torch.nn import Module, Linear, Conv2d, Sequential, MaxPool2d, LeakyReLU
-from torch.nn.functional import leaky_relu
-from torch.utils import model_zoo
+"""
+DCGAN code taken from:
+https://github.com/sdhnshu/pytorch-model-zoo/blob/master/dcgan/model.py
+Because they had a fairly well written, simple version available.
+"""
+
+import torch.nn as nn
+import torch.nn.functional as F
 
 from utility import gpu, seed_all
 
 
-model_urls = {
-    'alexnet': 'https://download.pytorch.org/models/alexnet-owt-4df8aa71.pth',
-}
+def deconv(c_in, c_out, k_size, stride=2, pad=1, bn=True):
+    layers = []
+    layers.append(nn.ConvTranspose2d(c_in, c_out, k_size, stride, pad))
+    if bn:
+        layers.append(nn.BatchNorm2d(c_out))
+    return nn.Sequential(*layers)
 
 
-class AlexNet(Module):
-    """
-    Slightly modified version from here: https://github.com/pytorch/vision/blob/master/torchvision/models/alexnet.py
-    """
-    def __init__(self):
-        super().__init__()
-        self.features = Sequential(
-            Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
-            LeakyReLU(inplace=True),
-            MaxPool2d(kernel_size=3, stride=2),
-            Conv2d(64, 192, kernel_size=5, padding=2),
-            LeakyReLU(inplace=True),
-            MaxPool2d(kernel_size=3, stride=2),
-            Conv2d(192, 384, kernel_size=3, padding=1),
-            LeakyReLU(inplace=True),
-            Conv2d(384, 256, kernel_size=3, padding=1),
-            LeakyReLU(inplace=True),
-            Conv2d(256, 256, kernel_size=3, padding=1),
-            LeakyReLU(inplace=True),
-            MaxPool2d(kernel_size=3, stride=2),
-        )
-        self.classifier = Sequential(
-            Linear(256 * 6 * 6, 4096),
-            LeakyReLU(inplace=True),
-            Linear(4096, 4096),
-            LeakyReLU(inplace=True),
-            Linear(4096, 1),
-        )
+def conv(c_in, c_out, k_size, stride=2, pad=1, bn=True):
+    layers = []
+    layers.append(nn.Conv2d(c_in, c_out, k_size, stride, pad))
+    if bn:
+        layers.append(nn.BatchNorm2d(c_out))
+    return nn.Sequential(*layers)
+
+
+class Generator(nn.Module):
+    def __init__(self, z_dim=256, image_size=128, conv_dim=64):
+        seed_all(0)
+        super(Generator, self).__init__()
+        self.fc = deconv(z_dim, conv_dim * 8,
+                         int(image_size / 16), 1, 0, bn=False)
+        self.deconv1 = deconv(conv_dim * 8, conv_dim * 4, 4)
+        self.deconv2 = deconv(conv_dim * 4, conv_dim * 2, 4)
+        self.deconv3 = deconv(conv_dim * 2, conv_dim, 4)
+        self.deconv4 = deconv(conv_dim, 3, 4, bn=False)
+
+    def forward(self, z):
+        z = z.view(z.size(0), z.size(1), 1, 1)
+        out = self.fc(z)                            # (?, 512, 4, 4)
+        out = F.leaky_relu(self.deconv1(out), 0.05)  # (?, 256, 8, 8)
+        out = F.leaky_relu(self.deconv2(out), 0.05)  # (?, 128, 16, 16)
+        out = F.leaky_relu(self.deconv3(out), 0.05)  # (?, 64, 32, 32)
+        out = F.tanh(self.deconv4(out))             # (?, 3, 64, 64)
+        return out
+
+
+class Discriminator(nn.Module):
+    def __init__(self, image_size=128, conv_dim=64):
+        seed_all(0)
+        super(Discriminator, self).__init__()
+        self.conv1 = conv(3, conv_dim, 4, bn=False)
+        self.conv2 = conv(conv_dim, conv_dim * 2, 4)
+        self.conv3 = conv(conv_dim * 2, conv_dim * 4, 4)
+        self.conv4 = conv(conv_dim * 4, conv_dim * 8, 4)
+        self.conv5 = conv(conv_dim * 8, 1, int(image_size / 16), 1, 0, False)
 
     def forward(self, x):
-        x = self.features(x)
-        x = x.view(x.size(0), 256 * 6 * 6)
-        x = self.classifier(x)
-        return x
-
-
-def alexnet(pretrained=False):
-    """AlexNet model architecture from the
-    `"One weird trick..." <https://arxiv.org/abs/1404.5997>`_ paper.
-    Args:
-        pretrained (bool): If True, returns a model pre-trained on ImageNet
-    """
-    model = AlexNet()
-    if pretrained:
-        model.load_state_dict(model_zoo.load_url(model_urls['alexnet']))
-    return model
-
-
-class Generator(Module):
-    """The generator model."""
-    def __init__(self):
-        super().__init__()
-        self.input_size = 10
-        self.linear1 = Linear(self.input_size, 20)
-        self.linear5 = Linear(20, 30)
-        self.linear6 = Linear(30, observation_count * irrelevant_data_multiplier)
-
-    def forward(self, x, add_noise=False):
-        """The forward pass of the module."""
-        x = leaky_relu(self.linear1(x))
-        x = leaky_relu(self.linear5(x))
-        x = self.linear6(x)
-        return x
+        out = F.leaky_relu(self.conv1(x), 0.05)    # (?, 64, 32, 32)
+        out = F.leaky_relu(self.conv2(out), 0.05)  # (?, 128, 16, 16)
+        out = F.leaky_relu(self.conv3(out), 0.05)  # (?, 256, 8, 8)
+        out = F.leaky_relu(self.conv4(out), 0.05)  # (?, 512, 4, 4)
+        out = F.sigmoid(self.conv5(out)).squeeze()
+        return out
