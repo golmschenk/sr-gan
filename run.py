34e2cea433d73a53891b6bbe3fe486622a52e6d7
Greg Olmschenk <golmschenk@gmail.com>
2018-10-09 10:42:11 -0400

    Set the new default run settings

--- run.py
+++ run.py
@@ -43,18 +43,16 @@ else:
     raise ValueError('{} is not an available application.'.format(application_name))
 settings_.unlabeled_dataset_size = None
 settings_.labeled_dataset_size = [40]
-settings_.summary_step_period = 1000
+settings_.summary_step_period = 5000
 settings_.labeled_dataset_seed = [0]
 settings_.steps_to_run = 1000000
 settings_.learning_rate = [1e-4]
-settings_.gradient_penalty_multiplier = [10]
+settings_.gradient_penalty_multiplier = [0.1]
 settings_.mean_offset = [0]
 settings_.unlabeled_loss_order = 2
 settings_.fake_loss_order = 0.5
 settings_.generator_loss_order = 2
 # settings_.load_model_path = 'logs/base'
-settings_.number_of_data_workers = 6
-settings_.pin_memory = True
 settings_.local_setup()
 settings_list = convert_to_settings_list(settings_)
 seed_all(0)
