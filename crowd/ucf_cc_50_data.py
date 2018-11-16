790399e429c970257b33e9c90b36843d584de5d0
Greg Olmschenk <golmschenk@gmail.com>
2018-11-16 16:00:06 -0500

    Add proper UCF CC 50 handling for new network

--- crowd/ucf_cc_50_data.py
+++ crowd/ucf_cc_50_data.py
@@ -9,10 +9,11 @@ import imageio
 import numpy as np
 import patoolib
 import scipy.io
+import torchvision
 from torch.utils.data import Dataset
 
-from crowd.data import CrowdExample
-from crowd.label_generation import generate_density_label
+from crowd.data import CrowdExample, ExtractPatchForPosition, NegativeOneToOneNormalizeImage, NumpyArraysToTorchTensors
+from crowd.label_generation import generate_density_label, generate_point_density_map, generate_knn_map
 from utility import seed_all
 
 dataset_name = 'UCF CC 50'
@@ -22,22 +23,15 @@ else:
     database_directory = '../{}'.format(dataset_name)
 
 
-class UcfCc50Dataset(Dataset):
-    """
-    A class for the UCF CC 50 crowd dataset.
-    """
-    def __init__(self, transform=None, seed=None, number_of_examples=None,
-                 fake_dataset_length=False):
+class UcfCc50FullImageDataset(Dataset):
+    def __init__(self, seed=None, number_of_examples=None, start=0,
+                 map_directory_name='knn_maps'):
         seed_all(seed)
-        self.dataset_directory = database_directory
+        self.dataset_directory = os.path.join(database_directory)
         self.file_names = [name for name in os.listdir(os.path.join(self.dataset_directory, 'labels'))
-                           if name.endswith('.npy')][:number_of_examples]
-        self.fake_dataset_length = fake_dataset_length
-        if self.fake_dataset_length:
-            self.length = int(1e6)
-        else:
-            self.length = len(self.file_names)
-        self.transform = transform
+                           if name.endswith('.npy')][start:start + number_of_examples]
+        self.length = len(self.file_names)
+        self.map_directory_name = map_directory_name
 
     def __getitem__(self, index):
         """
@@ -46,17 +40,80 @@ class UcfCc50Dataset(Dataset):
         :return: An example and label from the crowd dataset.
         :rtype: torch.Tensor, torch.Tensor
         """
-        if self.fake_dataset_length:
-            random_index = random.randrange(len(self.file_names))
-            file_name = self.file_names[random_index]
-        else:
-            file_name = self.file_names[index]
+        file_name = self.file_names[index]
         image = np.load(os.path.join(self.dataset_directory, 'images', file_name))
         label = np.load(os.path.join(self.dataset_directory, 'labels', file_name))
-        example = CrowdExample(image=image, label=label)
-        if self.transform:
-            example = self.transform(example)
-        return example.image, example.label
+        knn_map = np.load(os.path.join(self.dataset_directory, self.map_directory_name, file_name))
+        return image, label, knn_map
+
+    def __len__(self):
+        return self.length
+
+
+class UcfCc50TransformedDataset(Dataset):
+    """
+    A class for the transformed UCF-CC-50 crowd dataset.
+    """
+
+    def __init__(self, image_patch_size=224, label_patch_size=224, seed=None, start=0,
+                 number_of_examples=None, middle_transform=None, inverse_map=False, map_directory_name='knn_maps'):
+        seed_all(seed)
+        self.dataset_directory = os.path.join(database_directory)
+        self.file_names = [name for name in os.listdir(os.path.join(self.dataset_directory, 'labels'))
+                           if name.endswith('.npy')][start:start + number_of_examples]
+        self.image_patch_size = image_patch_size
+        self.label_patch_size = label_patch_size
+        half_patch_size = int(self.image_patch_size // 2)
+        self.length = 0
+        self.start_indexes = []
+        for file_name in self.file_names:
+            self.start_indexes.append(self.length)
+            image = np.load(os.path.join(self.dataset_directory, 'images', file_name), mmap_mode='r')
+            y_positions = range(half_patch_size, image.shape[0] - half_patch_size + 1)
+            x_positions = range(half_patch_size, image.shape[1] - half_patch_size + 1)
+            image_indexes_length = len(y_positions) * len(x_positions)
+            self.length += image_indexes_length
+        self.middle_transform = middle_transform
+        self.inverse_map = inverse_map
+        self.map_directory_name = map_directory_name
+
+    def __getitem__(self, index):
+        """
+        :param index: The index within the entire dataset.
+        :type index: int
+        :return: An example and label from the crowd dataset.
+        :rtype: torch.Tensor, torch.Tensor
+        """
+        index_ = random.randrange(self.length)
+        file_name_index = np.searchsorted(self.start_indexes, index_, side='right') - 1
+        start_index = self.start_indexes[file_name_index]
+        file_name = self.file_names[file_name_index]
+        position_index = index_ - start_index
+        extract_patch_transform = ExtractPatchForPosition(self.image_patch_size, self.label_patch_size,
+                                                          allow_padded=True)  # In case image is smaller than patch.
+        preprocess_transform = torchvision.transforms.Compose([NegativeOneToOneNormalizeImage(),
+                                                               NumpyArraysToTorchTensors()])
+        image = np.load(os.path.join(self.dataset_directory, 'images', file_name), mmap_mode='r')
+        label = np.load(os.path.join(self.dataset_directory, 'labels', file_name), mmap_mode='r')
+        knn_map = np.load(os.path.join(self.dataset_directory, self.map_directory_name, file_name), mmap_mode='r')
+        if '1nn' in self.map_directory_name and 'i1nn' not in self.map_directory_name:
+            knn_map = knn_map / 112
+        half_patch_size = int(self.image_patch_size // 2)
+        y_positions = range(half_patch_size, image.shape[0] - half_patch_size + 1)
+        x_positions = range(half_patch_size, image.shape[1] - half_patch_size + 1)
+        positions_shape = [len(y_positions), len(x_positions)]
+        y_index, x_index = np.unravel_index(position_index, positions_shape)
+        y = y_positions[y_index]
+        x = x_positions[x_index]
+        example = CrowdExample(image=image, label=label, knn_map=knn_map)
+        example = extract_patch_transform(example, y, x)
+        if self.middle_transform:
+            example = self.middle_transform(example)
+        example = preprocess_transform(example)
+        map = example.knn_map
+        if self.inverse_map:
+            map = 1 / (map + 1)
+        return example.image, example.label, map
 
     def __len__(self):
         return self.length
@@ -90,8 +147,7 @@ class UcfCc50Preprocessing:
 
         print('Done downloading.')
 
-    @staticmethod
-    def preprocess():
+    def preprocess(self):
         """Preprocesses the database to a format with each label and image being it's own file."""
         images_directory = os.path.join(database_directory, 'images')
         labels_directory = os.path.join(database_directory, 'labels')
@@ -111,9 +167,25 @@ class UcfCc50Preprocessing:
             label_size = image.shape[:2]
             mat = scipy.io.loadmat(mat_path)
             head_positions = mat['annPoints']  # x, y ordering.
-            label = generate_density_label(head_positions, label_size)
+            head_positions = self.get_y_x_head_positions(head_positions)
+            for k in [1, 2, 3, 4, 5]:
+                knn_maps_directory = os.path.join(database_directory, '{}nn_maps'.format(k))
+                iknn_maps_directory = os.path.join(database_directory, 'i{}nn_maps'.format(k))
+                os.makedirs(knn_maps_directory, exist_ok=True)
+                os.makedirs(iknn_maps_directory, exist_ok=True)
+                knn_map_path = os.path.join(knn_maps_directory, file_name + 'npy')
+                iknn_map_path = os.path.join(iknn_maps_directory, file_name + 'npy')
+                knn_map = generate_knn_map(head_positions, label_size, number_of_neighbors=k, upper_bound=112)
+                iknn_map = 1 / (knn_map + 1)
+                np.save(iknn_map_path, iknn_map)
+                np.save(knn_map_path, knn_map)
+            label = generate_point_density_map(head_positions, label_size)
             np.save(image_path, image)
             np.save(label_path, label)
+
+    @staticmethod
+    def get_y_x_head_positions(original_head_positions):
+        return original_head_positions[:, [1, 0]]
 
 
 class UcfCc50Check:
@@ -125,7 +197,7 @@ class UcfCc50Check:
         """
         print('=' * 50)
         print('UCF CC 50')
-        dataset = UcfCc50Dataset()
+        dataset = UcfCc50FullImageDataset()
         label_sums = []
         for image, label in dataset:
             label_sums.append(label.sum())
