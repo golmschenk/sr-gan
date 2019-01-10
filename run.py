f492fba4ed66c250e8ae607423cdebf1d6c2f98a
Greg Olmschenk <golmschenk@gmail.com>
2019-01-10 12:29:46 -0500

    Update map multiplier in default run settings to prevent over emphasis on kNN labels

--- run.py
+++ run.py
@@ -33,8 +33,8 @@ elif application_name == 'coef':
     settings_.batch_size = 1000
 elif application_name == 'crowd':
     Experiment = {'srgan': CrowdExperiment, 'sgan': CrowdSganExperiment, 'dnn': CrowdDnnExperiment}[method_name]
-    settings_.unlabeled_loss_multiplier = [1e-1]
-    settings_.fake_loss_multiplier = [1e-1]
+    settings_.unlabeled_loss_multiplier = [1e-2, 1e0, 1e2]
+    settings_.fake_loss_multiplier = [1e-2, 1e0, 1e2]
     settings_.batch_size = 8
     settings_.number_of_cameras = [5]
     settings_.number_of_images_per_camera = [5]
@@ -55,12 +55,12 @@ settings_.fake_loss_order = 0.5
 settings_.generator_loss_order = 2
 settings_.load_model_path = 'logs/gan_quick_start'
 settings_.map_directory_name = ['i1nn_maps']
-settings_.map_multiplier = 1
+settings_.map_multiplier = 1e-6
 settings_.local_setup()
 settings_list = convert_to_settings_list(settings_, shuffle=True)
 seed_all(0)
 for settings_ in settings_list:
-    trial_name = 'gp {}'.format(settings_.map_directory_name)
+    trial_name = '{}'.format(settings_.map_directory_name)
     trial_name += ' {}'.format(application_name)
     trial_name += ' {}'.format(method_name) if method_name != 'srgan' else ''
     if application_name == 'crowd' and settings_.crowd_dataset == 'World Expo':
@@ -73,6 +73,7 @@ for settings_ in settings_list:
     trial_name += ' gp{:e}'.format(settings_.gradient_penalty_multiplier)
     trial_name += ' mo{:e}'.format(settings_.mean_offset)
     trial_name += ' lr{:e}'.format(settings_.learning_rate)
+    trial_name += ' mm{:e}'.format(settings_.map_multiplier)
     trial_name += ' gs{}'.format(settings_.generator_training_step_period)
     trial_name += ' ls{}'.format(settings_.labeled_dataset_seed)
     trial_name += ' u{}f{}g{}'.format(settings_.unlabeled_loss_order,
