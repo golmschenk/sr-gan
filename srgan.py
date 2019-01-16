ca826134a654f0bc56fc495a6b9152dd92e8997a
Greg Olmschenk <golmschenk@gmail.com>
2019-01-16 13:50:42 -0500

    Use matching distance loss (instead of divergent distance loss) for gradient penalty

--- srgan.py
+++ srgan.py
@@ -347,9 +347,8 @@ class Experiment(ABC):
         """Calculates the interpolate loss for use in the gradient penalty."""
         _ = self.D(interpolates)
         self.interpolates_features = self.D.features
-        interpolates_loss = feature_distance_loss(self.unlabeled_features, self.interpolates_features,
-                                                  distance_function=self.settings.fake_loss_distance
-                                                  ).neg() * self.settings.fake_loss_multiplier
+        interpolates_loss = feature_distance_loss(self.unlabeled_features, self.interpolates_features
+                                                  ) * self.settings.fake_loss_multiplier
         return interpolates_loss
 
     def generator_loss_calculation(self, fake_examples, unlabeled_examples):
